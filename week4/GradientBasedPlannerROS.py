c41642f17ff62df7c236387c4620a4998fa84978
RuslanAgishev <agishev_ruslan@mail.ru>
2019-01-18 16:59:49 +0300

    local

--- week4/GradientBasedPlannerROS.py
+++ week4/GradientBasedPlannerROS.py
@@ -188,52 +188,52 @@ if toFly:
     cf1.takeoff(targetHeight = TakeoffHeight, duration = TakeoffTime)
     time.sleep(TakeoffTime+1)
 
-    cf1.land(targetHeight = 0.0, duration = 2.0)
-    time.sleep(3.0)
+    # cf1.land(targetHeight = 0.0, duration = 2.0)
+    # time.sleep(3.0)
 
-    # rate = rospy.Rate(40)
+    rate = rospy.Rate(40)
 
-    # msg = Position()
-    # msg.header.seq = 0
-    # msg.header.stamp = rospy.Time.now()
-    # msg.header.frame_id = "/world"
+    msg = Position()
+    msg.header.seq = 0
+    msg.header.stamp = rospy.Time.now()
+    msg.header.frame_id = "/world"
 
-    # cfname = cf_names[0]
-    # pub = rospy.Publisher(cfname+"/cmd_position", Position, queue_size=1)
-    # stop_pub = rospy.Publisher(cfname+"/cmd_stop", Empty, queue_size=1)
-    # stop_msg = Empty()
-    # pos_sub = rospy.Subscriber('/vicon/'+cf_names[0]+'/'+cf_names[0], TransformStamped, pos_callback)
+    cfname = cf_names[0]
+    pub = rospy.Publisher(cfname+"/cmd_position", Position, queue_size=1)
+    stop_pub = rospy.Publisher(cfname+"/cmd_stop", Empty, queue_size=1)
+    stop_msg = Empty()
+    pos_sub = rospy.Subscriber('/vicon/'+cf_names[0]+'/'+cf_names[0], TransformStamped, pos_callback)
 
-    # start_time = rospy.Time.now()
-    # traj = route
+    start_time = rospy.Time.now()
+    traj = route
 
-    # if direction=='inverse':
-    #     RANGE = reversed(range(len(traj)))
-    # else:
-    #     RANGE = range(len(traj))
-    # for i in RANGE:
-    #     msg.header.seq += 1
-    #     msg.header.stamp = rospy.Time.now()
-    #     t = (msg.header.stamp - start_time).to_sec()
+    if direction=='inverse':
+        RANGE = reversed(range(len(traj)))
+    else:
+        RANGE = range(len(traj))
+    for i in RANGE:
+        msg.header.seq += 1
+        msg.header.stamp = rospy.Time.now()
+        t = (msg.header.stamp - start_time).to_sec()
         
-    #     msg.x    = traj[i,0]
-    #     msg.y    = traj[i,1]
-    #     msg.z    = TakeoffHeight
-    #     msg.yaw  = 0
+        msg.x    = traj[i,0]
+        msg.y    = traj[i,1]
+        msg.z    = TakeoffHeight
+        msg.yaw  = 0
 
-    #     pub.publish(msg)
-    #     print(msg)
-    #     rate.sleep()
+        pub.publish(msg)
+        print(msg)
+        rate.sleep()
 
-    # rospy.loginfo('Landing...')
-    # while msg.z > -1.0:
-    #     msg.z -= 0.02
-    #     # if ( cf_pos.transform.translation.z < 0.11 ):
-    #     #     break
-    #     msg.header.seq += 1
-    #     msg.header.stamp = rospy.Time.now()
-    #     pub.publish(msg)
-    #     rate.sleep()
-    # stop_pub.publish(stop_msg)
+    rospy.loginfo('Landing...')
+    while msg.z > -1.0:
+        msg.z -= 0.02
+        # if ( cf_pos.transform.translation.z < 0.11 ):
+        #     break
+        msg.header.seq += 1
+        msg.header.stamp = rospy.Time.now()
+        pub.publish(msg)
+        rate.sleep()
+    stop_pub.publish(stop_msg)
 
 
