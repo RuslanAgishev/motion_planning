d6f227c3e3a69f125226c41f1a8e8a34d95997e2
RuslanAgishev <agishev_ruslan@mail.ru>
2019-01-23 16:37:58 +0300

    formation control

--- python_src/gradient_online.py
+++ python_src/gradient_online.py
@@ -1,27 +1,19 @@
 # python3 gradient_online.py
 
 import numpy as np
+from numpy.linalg import norm
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 from scipy.ndimage.morphology import distance_transform_edt as bwdist
 from math import *
 
 from progress.bar import FillingCirclesBar
 
 
-
-""" init """
-animate = True
-max_its = 800
-random_map = 1
-num_random_obstacles = 4
-moving_obstacles = 1
-progress_bar = FillingCirclesBar('Simulation Progress', max=max_its)
-R_obstacles = 0.1 # [m]
-R_swarm     = 0.2 # [m]
-
 def draw_map(R_obstacles, nrows=500, ncols=500):
     skip = 10
     [x_m, y_m] = np.meshgrid(np.linspace(-2.5, 2.5, ncols), np.linspace(-2.5, 2.5, nrows))
+    [gy, gx] = np.gradient(-f);
     Q = plt.quiver(x_m[::skip, ::skip], y_m[::skip, ::skip], gx[::skip, ::skip], gy[::skip, ::skip])
     plt.plot(start[0], start[1], 'ro', markersize=10);
     plt.plot(goal[0], goal[1], 'ro', color='green', markersize=10);
@@ -31,6 +23,17 @@ def draw_map(R_obstacles, nrows=500, ncols=500):
     for pose in obstacles_poses:
         circle = plt.Circle(pose, R_obstacles, color='yellow')
         ax.add_artist(circle)
+    # Create a Rectangle patch
+    # rect = patches.Rectangle((0.1,0.1),0.5,0.5,linewidth=1,edgecolor='r',fill='True')
+    # ax.add_patch(rect)
+
+def draw_robots():
+    plt.arrow(current_point[0], current_point[1], V[0], V[1], width=0.01, head_width=0.05, head_length=0.1, fc='k')
+    plt.plot(route1[:,0], route1[:,1], 'red', linewidth=2)
+    plt.plot(route2[:,0], route2[:,1], '--', linewidth=2)
+    plt.plot(route3[:,0], route3[:,1], '--', linewidth=2)
+    triangle = plt.Polygon([next_point1, next_point2, next_point3], color='green', fill=False, linewidth=2);
+    plt.gca().add_patch(triangle)
 
 def meters2grid(pose_m, nrows=500, ncols=500):
     # [0, 0](m) -> [250, 250]
@@ -38,7 +41,6 @@ def meters2grid(pose_m, nrows=500, ncols=500):
     # [0,-1](m) -> [250, 250-100]
     pose_on_grid = np.array(pose_m)*100 + np.array([ncols/2, nrows/2])
     return np.array( pose_on_grid, dtype=int)
-
 def grid2meters(pose_grid, nrows=500, ncols=500):
     # [250, 250] -> [0, 0](m)
     # [250+100, 250] -> [1, 0](m)
@@ -53,13 +55,14 @@ def gradient_planner(f, current_point, end_coords):
 	"""
     [gy, gx] = np.gradient(-f);
     iy, ix = np.array( meters2grid(current_point), dtype=int )
-    w = 40 # smoothing window size for gradient-velocity
+    w = 80 # smoothing window size for gradient-velocity
     vx = np.mean(gx[ix-int(w/2) : ix+int(w/2), iy-int(w/2) : iy+int(w/2)])
-    vy = np.mean(gy[ix-int(w/2) : ix+int(w/2), iy-int(w/2) : iy+int(w/2)])        
-    dt = 0.1 / np.linalg.norm([vx, vy]);
+    vy = np.mean(gy[ix-int(w/2) : ix+int(w/2), iy-int(w/2) : iy+int(w/2)])
+    V = np.array([vx, vy])
+    dt = 0.1 / norm(V);
     next_point = current_point + dt*np.array( [vx, vy] );
 
-    return next_point
+    return next_point, V
 
 def combined_potential(obstacles_poses, goal, R_obstacles, nrows=500, ncols=500):
     """ Obstacles map """
@@ -72,7 +75,7 @@ def combined_potential(obstacles_poses, goal, R_obstacles, nrows=500, ncols=500)
         t = ((x - x0)**2 + (y - y0)**2) < (100*R_obstacles)**2
         obstacles_map[t] = True;
     # rectangular obstacles
-    obstacles_map[300:, 230:250] = True;
+    obstacles_map[350:, 130:150] = True;
     obstacles_map[130:150, :300] = True;
     """ Repulsive potential """
     goal = meters2grid(goal)
@@ -83,16 +86,35 @@ def combined_potential(obstacles_poses, goal, R_obstacles, nrows=500, ncols=500)
     repulsive = nu*((1./d2 - 1/d0)**2);
     repulsive [d2 > d0] = 0;
     """ Attractive potential """
-    xi = 1/700.;
+    xi = 1/1000.;
     attractive = xi * ( (x - goal[0])**2 + (y - goal[1])**2 );
     """ Combine terms """
     f = attractive + repulsive;
     return f
 
+def move_obstacles(obstacles_poses):
+    dx = 0.01;                   dy = 0.01
+    obstacles_poses[0][0] += dx; obstacles_poses[0][1] -= dy
+    obstacles_poses[1][0] -= dx; obstacles_poses[1][1] -= dy
+    obstacles_poses[2][0] -= dx; obstacles_poses[2][1] -= dy
+    obstacles_poses[3][0] += dx; obstacles_poses[3][1] += dy
+    return obstacles_poses
 
 
+""" initialization """
+animate = 1
+max_its = 100
+random_map = 0
+num_random_obstacles = 4
+moving_obstacles = 1
+progress_bar = FillingCirclesBar('Simulation Progress', max=max_its)
+R_obstacles = 0.1 # [m]
+R_swarm     = 0.2 # [m]
+start = np.array([-2.0, 2.0]); goal = np.array([2.0, -2.0])
+V0 = (goal - start) / norm(goal-start)   # initial movement direction, |V0| = 1
+U0 = np.array([-V0[1], V0[0]]) / norm(V0) # perpendicular to initial movement direction, |U0|=1
 
-start = np.array([-1.5, 0.5]); goal = np.array([1.5, -1.0]);
+
 if random_map:
     obstacles_poses = np.random.uniform(low=-2.5, high=2.5, size=(num_random_obstacles,2)) # randomly located obstacles 
 else:
@@ -103,35 +125,50 @@ else:
 
 """ Plan route: centroid path """
 plt.figure(figsize=(10, 10))
-route = start
+# drones forming equilateral triangle
+route1 = start # leader
+route2 = route1 - V0*(R_swarm*sqrt(3)/2) + U0*R_swarm/2 # follower
+route3 = route1 - V0*(R_swarm*sqrt(3)/2) - U0*R_swarm/2 # follower
 current_point = start
+
 for i in range(max_its):
-    if moving_obstacles:
-        dx = 0.03;                   dy = 0.03
-        obstacles_poses[0][0] += dx; obstacles_poses[0][1] -= dy
-        obstacles_poses[1][0] -= dx; obstacles_poses[1][1] -= dy
-        obstacles_poses[2][0] -= dx; obstacles_poses[2][1] -= dy
-        obstacles_poses[3][0] += dx; obstacles_poses[3][1] += dy
+    if moving_obstacles: obstacles_poses = move_obstacles(obstacles_poses)
+
     f = combined_potential(obstacles_poses, goal, R_obstacles)
-    [gy, gx] = np.gradient(-f);
-    dist_to_goal = np.linalg.norm(current_point - goal)
+    dist_to_goal = norm(current_point - goal)
     if dist_to_goal < 0.1:
         print('\nReached the goal')
         break
-    next_point = gradient_planner(f, current_point, goal)
-    route = np.vstack([route, next_point])
-    current_point = next_point
+    # drones forming equilateral triangle
+    next_point1, V = gradient_planner(f, current_point, goal)
+    U = np.array([-V[1], V[0]]) # perpendicular to the movement direction
+    # scale_min * triangular formation < triangular formation < scale_max * triangular formation
+    scale_min = 0.6; scale_max = 1.5
+    if norm(V) < scale_min:
+        v = scale_min*V / norm(V); u = scale_min*U / norm(V)
+    elif norm(V) > scale_max:
+        v = scale_max*V / norm(V); u = scale_max*U / norm(V)
+    else:
+        v = V; u = U
+    next_point2 = next_point1 - v*R_swarm*sqrt(3)/2 + u*R_swarm/2 # follower
+    next_point3 = next_point1 - v*R_swarm*sqrt(3)/2 - u*R_swarm/2 # follower
+
+    route1 = np.vstack([route1, next_point1])
+    route2 = np.vstack([route2, next_point2])
+    route3 = np.vstack([route3, next_point3])
+
+    current_point = next_point1
 
     progress_bar.next()
     plt.cla()
     draw_map(R_obstacles)
-    plt.plot(route[:,0], route[:,1], 'blue', linewidth=2);
+    draw_robots()
     if animate:
         plt.draw()
         plt.pause(0.01)
 
 
-print('Done')
+print('\nDone')
 progress_bar.finish()
 plt.show()
 
