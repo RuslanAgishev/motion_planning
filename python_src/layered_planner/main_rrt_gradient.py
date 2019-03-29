934374951780f2f85a51c30af592b97fe764b0c6
RuslanAgishev <agishev_ruslan@mail.ru>
2019-03-29 16:35:08 +0300

    minor changes: local planner function

--- python_src/layered_planner/main_rrt_gradient.py
+++ python_src/layered_planner/main_rrt_gradient.py
@@ -29,10 +29,24 @@ class Params:
         self.world_bounds_y = [-2.5, 2.5] # [m], map size in Y-direction
         self.drone_vel = 2.0 # [m/s]
         self.ViconRate = 100 # [Hz]
-        self.max_sp_dist = 1.0 * self.drone_vel # [m], maximum distance between current robot's pose and the sp from global planner
+        self.max_sp_dist = 0.5 * self.drone_vel # [m], maximum distance between current robot's pose and the sp from global planner
         self.influence_radius = 1.3 # potential fields radius, defining repulsive area size near the obstacle
         self.goal_tolerance = 0.05 # [m], maximum distance threshold to reach the goal
         self.num_robots = 4
+
+class Robot:
+    def __init__(self):
+        self.sp = [0,0]
+        self.sp_global = [0,0]
+        self.route = np.array([self.sp])
+        self.f = 0
+        self.leader = False
+
+    def local_planner(self, obstacles, params):
+        obstacles_grid = grid_map(obstacles)
+        self.f = combined_potential(obstacles_grid, self.sp_global, params.influence_radius)
+        self.sp = gradient_planner_next(self.sp, self.f, params)
+        self.route = np.vstack( [self.route, self.sp] )
 
 # Initialization
 params = Params()
@@ -59,75 +73,64 @@ obstacles = [
               np.array([[0.0, -2.3], [0.1, -2.3], [0.1, -2.2], [0.0, -2.2]]),
             ]
 
-# plt.figure(figsize=(12,12))
-# plt.grid()
-# draw_map(obstacles)
-# plt.plot(xy_start[0],xy_start[1],'bo',color='red', markersize=10, label='start')
-# plt.plot(xy_goal[0], xy_goal[1],'bo',color='green', markersize=10, label='goal')
-# plt.legend()
+robots = []
+for i in range(params.num_robots):
+    robots.append(Robot())
+robot1 = robots[0]; robot1.leader=True
 
-
-# Global Planner: RRT path
-# plt.figure(figsize=(10,10))
-# draw_map(obstacles)
-# plt.plot(xy_start[0],xy_start[1],'bo',color='red', markersize=20)
-# plt.plot(xy_goal[0], xy_goal[1],'bo',color='green', markersize=20)
 
 P_long = rrt_path(obstacles, xy_start, xy_goal, params)
 P = ShortenPath(P_long, obstacles, smoothiters=30) # P = [[xN, yN], ..., [x1, y1], [x0, y0]]
 traj_global = waypts2setpts(P, params)
-
-# plt.plot( P_long[:,0], P_long[:,1], color='blue', linewidth=5, label='RRT path' )
-# plt.plot(P[:,0], P[:,1], linewidth=5, color='orange', label='shortened path')
-# plt.legend()
-# plt.grid()
 
 
 # Layered Motion Planning: RRT (global) + Potential Field (local)
 if __name__ == '__main__':
     plt.figure(figsize=(10,10))
     sp_ind = 0
-    route = np.array([traj_global[0,:]])
-    sp1 = route[-1,:]
-    followers_sp = formation(params.num_robots, leader_des=sp1, v=np.array([0,-0.3]), l=0.3)
+    robot1.route = np.array([traj_global[0,:]])
+    robot1.sp = robot1.route[-1,:]
+
+    followers_sp = formation(params.num_robots, leader_des=robot1.sp, v=np.array([0,-0.3]), l=0.3)
+    for i in range(len(followers_sp)):
+        robots[i+1].sp = followers_sp[i]
+        robots[i+1].route = np.array([followers_sp[i]])
+
     while True: # loop through all the setpoint from global planner trajectory, traj_global
-        
-        dist_to_goal = norm(sp1-xy_goal)
+        dist_to_goal = norm(robot1.sp - xy_goal)
         if dist_to_goal < params.goal_tolerance: # [m]
             print 'Goal is reached'
             break
-        obstacles = move_obstacles(obstacles) # change poses of some obstacles on the map         
-        obstacles_grid = grid_map(obstacles)
+        obstacles = move_obstacles(obstacles) # change poses of some obstacles on the map
 
         # leader's setpoint from global planner
-        sp_global = traj_global[sp_ind,:]
-
+        robot1.sp_global = traj_global[sp_ind,:]
         # correct leader's pose with local planner
-        f1 = combined_potential(obstacles_grid, sp_global, params.influence_radius)
-        sp1 = gradient_planner_next(sp1, f1, params)
-        route = np.vstack( [route, sp1] )
+        robot1.local_planner(obstacles, params)
 
-        # adding following robots in the swarm:
-        followers_sp_global = formation(params.num_robots, leader_des=sp_global, v=normalize(sp_global-sp1), l=0.3) # formation poses from global planner
+        """ adding following robots in the swarm """
+        # formation poses from global planner
+        followers_sp_global = formation(params.num_robots, robot1.sp_global, v=normalize(robot1.sp_global-robot1.sp), l=0.3)
+        for i in range(len(followers_sp_global)): robots[i+1].sp_global = followers_sp_global[i]
         for p in range(len(followers_sp)): # formation poses correction with local planner
             # robots repel from each other inside the formation
-            robots_obstacles_sp = [x for i,x in enumerate(followers_sp + [sp1]) if i!=p] # all poses except the robot[p]
+            robots_obstacles_sp = [x for i,x in enumerate(followers_sp + [robot1.sp]) if i!=p] # all poses except the robot[p]
             robots_obstacles = poses2polygons( robots_obstacles_sp ) # each drone is defined as a small cube for inter-robots collision avoidance
             obstacles1 = np.array(obstacles + robots_obstacles) # combine exisiting obstacles on the map with other robots[for each i: i!=p] in formation
-            obstacles_grid1 = grid_map(obstacles1) # obstacles grid representation from polygons
-            # robot[p] position correction with local planner
-            f = combined_potential(obstacles_grid1, followers_sp_global[p], params.influence_radius)
-            followers_sp[p] = gradient_planner_next(followers_sp[p], f, params)
+            # follower robot's position correction with local planner
+            robots[p+1].local_planner(obstacles1, params)
+            followers_sp[p] = robots[p+1].sp
+
 
         # visualization
         plt.cla()
         draw_map(obstacles)
-        draw_gradient(f)
-        plt.plot(sp1[0], sp1[1], '^', color='blue', markersize=10, zorder=15) # leader's pose
-        for sp in followers_sp: plt.plot(sp[0], sp[1], '^', color='blue', markersize=10, zorder=15) # followers poses
-        plt.plot(route[:,0], route[:,1], linewidth=3, color='green', label="Robot's path, corrected with local planner", zorder=10)
+        draw_gradient(robots[2].f)
+        for robot in robots: plt.plot(robot.sp[0], robot.sp[1], '^', color='blue', markersize=10, zorder=15) # robots poses
+        plt.plot(robot1.route[:,0], robot1.route[:,1], linewidth=2, color='green', label="Robot's path, corrected with local planner", zorder=10)
+        for robot in robots[1:]: plt.plot(robot.route[:,0], robot.route[:,1], '--', linewidth=2, color='green', zorder=10)
         plt.plot(P[:,0], P[:,1], linewidth=3, color='orange', label='Global planner path')
-        plt.plot(traj_global[sp_ind,0], traj_global[sp_ind,1], 'ro', color='blue', markersize=5, label='Global planner setpoint')
+        plt.plot(traj_global[sp_ind,0], traj_global[sp_ind,1], 'ro', color='blue', markersize=7, label='Global planner setpoint')
         plt.plot(xy_start[0],xy_start[1],'bo',color='red', markersize=20, label='start')
         plt.plot(xy_goal[0], xy_goal[1],'bo',color='green', markersize=20, label='goal')
         plt.legend()
@@ -135,7 +138,7 @@ if __name__ == '__main__':
         plt.pause(0.01)
 
         # update loop variable
-        if sp_ind < traj_global.shape[0]-1 and norm(sp_global - sp1) < params.max_sp_dist: sp_ind += 1
+        if sp_ind < traj_global.shape[0]-1 and norm(robot1.sp_global - robot1.sp) < params.max_sp_dist: sp_ind += 1
 
 
     # close windows if Enter-button is pressed
